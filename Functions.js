2e577e116357273455b0b35f29c6b30221403e67
MGoradeCodes <153507645+MGoradeCodes@users.noreply.github.com>
2025-09-10 18:37:24 +0530

    Changes made

---
+++ Functions.js
@@ -0,0 +1,5 @@
+function Clear(){
+    document.getElementById("searchbox").value = "";
+    document.getElementById("searchbox").style.setProperty('--placeholder-color', 'white');
+    document.getElementById("searchbox").value = "Thankyou, Manthan will look into booking shortly!"
+}
